6fd27956e0c74d418ba492c55a53b8812d954ea5
abmargb <abmargb@gmail.com>
2014-04-15 13:10:13 -0300

    Added main.js

---
+++ public/scripts/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $('#btnLogin').click(function () {
+  	doLogin($('#inputJid').val(), $('#inputPassword').val());
+  });
+
+  $('#btnSendMessage').click(function () {
+  	sendMessage($('#inputMessage').val());
+  });
+
+  $('.list-group').empty();
+});
+
+var handleItems = function(items) {
+  items.forEach(function(item) {
+  	var content = "<li class=\"list-group-item\"><span " +  
+        "class=\"badge pull-left\">" + item.entry.atom.author.name + 
+        "</span><br><h4>" + item.entry.atom.content.content + "</h4></li>";
+    $('.list-group').append(content)
+  });
+};
